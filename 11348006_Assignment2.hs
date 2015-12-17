4a18cefe87b5ae61bb8274e151ff36ca428bbd9e
The-Royal-We <brendan.carroll.2012@nuim.ie>
2015-12-17 16:28:45 +0000

    Final working verison

--- 11348006_Assignment2.hs
+++ 11348006_Assignment2.hs
@@ -1,13 +1,14 @@
-module Assigment where
+module Dreidel where
 import Dist
 import Data.Ord
 import Data.List
 
-dreidelDreidelDreidel :: Double -> Double -> Int -> Dist Double
+dreidelDreidelDreidel :: Int -> Double -> Int -> Dist Double
 dreidelDreidelDreidel pot p n = do
   dreidelDreidelDreidel' p n (distributionOfPot)
     where
-      distributionOfPot = return pot :: Dist Double
+      potToDouble = realToFrac pot
+      distributionOfPot = return potToDouble :: Dist Double
 
 dreidelDreidelDreidel' :: Double -> Int -> Dist Double -> Dist Double
 dreidelDreidelDreidel' p 1 pxs = Dist ((computeWin pxs p) ++ (computeLoss pxs p))
@@ -24,24 +25,25 @@ computeLoss (Dist pxs) portion = [(x - (portion * x), p * 3/4) | (x,p) <- pxs ]
 mean :: Dist Double -> Double
 mean (Dist pxs) = sum [(x*p) | (x,p) <- pxs]
 
-maximizePotentialReturn :: Double
-maximizePotentialReturn = maximum [ (mean $ (dreidelDreidelDreidel 1000 x 10)) | x <- [0.1,0.2..1.0]]
-
-getMaxPortionAndProbability :: (Double, Double)
-getMaxPortionAndProbability =  maximumBy (comparing snd) $ zip [0.1, 0.2 .. 1.0 ] [prExceeds 4000 $ dreidelDreidelDreidel 1000 p 10 | p <-[0.1, 0.2 .. 1.0 ]]
+-- Find p which maximizes mean (dreidelDreidelDreidel 1000 p 10)
+-- 1.0
+pWhereMaximizesPotentialReturn :: Double
+pWhereMaximizesPotentialReturn = fst . maximumBy (comparing snd) $ zip [0.1, 0.2 .. 1.0]  [(mean $ (dreidelDreidelDreidel 1000 x 10)) | x <- [0.1,0.2..1.0]]
 
 -- Find p which maximizes prExceeds 4000 (dreidelDreidelDreidel 1000 p 10))
+-- 0.30000000000000004
 maximumPortion :: Double
-maximumPortion = fst . getMaxPortionAndProbability
+maximumPortion = fst . maximumBy (comparing snd) $ zip [0.1, 0.2 .. 1.0 ] [prExceeds 4000 $ dreidelDreidelDreidel 1000 p 10 | p <-[0.1, 0.2 .. 1.0 ]]
 
 -- What is the probability of ending up with over 4000 using that value of p?
+-- 0.4744071960449219
 probabilityOfExceeding :: Double
-probabilityOfExceeding = snd . getMaxPortionAndProbability
+probabilityOfExceeding = snd . maximumBy (comparing snd) $ zip [0.1, 0.2 .. 1.0 ] [prExceeds 4000 $ dreidelDreidelDreidel 1000 p 10 | p <-[0.1, 0.2 .. 1.0 ]]
 
 -- What is the expected (i.e., mean) amount ended up with from this p?
+-- 68029.68136433346
 meanAmountFromMaximum :: Double
 meanAmountFromMaximum = mean $ (dreidelDreidelDreidel 1000 maximumPortion 10)
 
 prExceeds :: Double -> Dist Double -> Double
 prExceeds target dist = mean $ fmap (fromIntegral . fromEnum . (>= target)) dist
-
