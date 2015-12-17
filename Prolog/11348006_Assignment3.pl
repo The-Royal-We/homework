2415d1c0fe33e3fe7efa676e5ec4dfa079fe64a0
The-Royal-We <brendan.carroll.2012@nuim.ie>
2015-12-17 17:56:28 +0000

    Added working version of assignment.

--- Prolog/11348006_Assignment3.pl
+++ Prolog/11348006_Assignment3.pl
@@ -1,5 +1,19 @@
+%% Interleave/3 question
 interleave([],[],[]).
 interleave([],L,L).
 interleave(L,[],L).
 interleave([H|T], L, [H|R]) :- interleave(T,L,R).
 interleave(T, [H|L], [H|R]) :- interleave(T,L,R).
+
+%% Ancestry Question
+male(me).
+female(sarah).
+parent(paul, me).
+parent(anne, me).
+parent(nora, anne).
+parent(noel, sarah).
+parent(nora, noel).
+
+grandParent(X, Z) :- parent(X, Y), parent(Y, Z).
+sibling(X,Z) :- parent(Y,X), parent(Y,Z).
+cousin(X,Z) :- grandParent(Y,X), grandParent(Y,Z).
