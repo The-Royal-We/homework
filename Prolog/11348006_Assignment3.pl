eb65729fa15a3653904f96c00457bda8f6ca8819
The-Royal-WE <brendan.carroll.92@gmail.com>
2015-12-22 14:46:09 +0000

    Added finished version of Prolog assignment.

--- Prolog/11348006_Assignment3.pl
+++ Prolog/11348006_Assignment3.pl
@@ -6,6 +6,8 @@ interleave([H|T], L, [H|R]) :- interleave(T,L,R).
 interleave(T, [H|L], [H|R]) :- interleave(T,L,R).
 
 %% Ancestry Question
+%%% Defined below for debugging purposes
+
 male(me).
 female(sarah).
 parent(paul, me).
@@ -17,3 +19,11 @@ parent(nora, noel).
 grandParent(X, Z) :- parent(X, Y), parent(Y, Z).
 sibling(X,Z) :- parent(Y,X), parent(Y,Z).
 cousin(X,Z) :- grandParent(Y,X), grandParent(Y,Z).
+/*
+* Using the above definition, siblings would indeed be delcared cousins,
+* as they share the same grandparent which satisfies the preconditions of
+* cousin/2. By the same logic I am also a cousin of myself.
+*
+* One way to avoid this behavior is to define cousin as a relation of my parents
+* siblings and the relation of those siblings to the arguments in cousin/2. 
+*/
